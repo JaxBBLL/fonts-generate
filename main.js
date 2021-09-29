c9eb3a8a91cb636cc05f8ed9e0f75abe8210badf
JaxBBLL <showcye@foxmail.com>
2021-09-29 15:02:12 +0800

    feat: update

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const Fontmin = require("fontmin");
 
-const { shuffle, getHexCode, random } = require("./util");
+const { shuffle, getHexCode, random, getHtml } = require("./util");
 
 const exportPath = "./fonts/" + Date.now();
 const shuffleArray = shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-const startCode = random(0xe01, 0xe001);
+const startCode = random(0xe001, 0xe801);
 
 const fontmin = new Fontmin()
   .src(shuffleArray.map((item) => `svgs/${item}.svg`))
@@ -28,13 +28,22 @@ fontmin.run(function (err, files) {
   if (err) {
     throw err;
   }
-  const codes = getHexCode(startCode);
+  const codes = getHexCode(startCode, shuffleArray.length);
   const result = {};
+  let txt = "";
   shuffleArray.forEach((item, index) => {
     result[item] = codes[index];
   });
+  shuffleArray.forEach((item, index) => {
+    txt += result[index];
+  });
   const str = JSON.stringify(result);
   fs.writeFile(exportPath + "/code.json", str, function (err) {
+    if (err) {
+      res.status(500).send("Server is error...");
+    }
+  });
+  fs.writeFile(exportPath + "/index.html", getHtml(txt), function (err) {
     if (err) {
       res.status(500).send("Server is error...");
     }
--- util.js
+++ util.js
@@ -7,12 +7,35 @@ function shuffle(arr) {
   return arr;
 }
 
-function getHexCode(startCode) {
+function getHexCode(startCode, len) {
   const res = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < len; i++) {
     res.push("&#x" + (startCode + i).toString(16) + ";");
   }
   return res;
+}
+
+function getHtml(str) {
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>font</title>
+      <link rel="stylesheet" href="font.css" />
+      <style>
+        .text {
+          font-family: iconnumber;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="text">${str}</div>
+    </body>
+  </html>
+  `;
+  return html;
 }
 
 function random(min, max) {
@@ -21,6 +44,7 @@ function random(min, max) {
 
 module.exports = {
   shuffle,
-  getHexCode,
   random,
+  getHexCode,
+  getHtml,
 };
